6f76bd3c35e6e6fc22cfde4a99289cb73b262e39
David Nidever <dnidever@lsst.org>
2017-05-10 21:38:44 -0700

    updates

--- dl/dldo.py
+++ dl/dldo.py
@@ -25,6 +25,10 @@ import getpass
 # Data Lab Client interfaces
 from dl import authClient, storeClient, queryClient
 
+# Service URLs
+AM_URL = "http://dlsvcs.datalab.noao.edu/auth"      # Auth Manager
+SM_URL = "http://dlsvcs.datalab.noao.edu/storage"   # Storage Manager
+QM_URL = "http://dlsvcs.datalab.noao.edu/query"     # Query Manager
 
 class Dldo:
     '''
@@ -36,7 +40,13 @@ class Dldo:
         self.loginstatus = ""
         self.unmount = ""
         pass
-        
+
+
+################################################
+#  Account Login Tasks
+################################################
+
+    
     def login(self, user):
         '''
         Login to datalab
@@ -90,15 +100,26 @@ class Dldo:
             print ("No user is currently logged into the Data Lab")
         else:
             print ("User %s is logged into the Data Lab" % self.user)
-        if self.mount != "":
-            if status != "loggedout":
-                print ("The user's Virtual Storage is mounted at %s" % self.mount)
-            else:
-                print ("The last user's Virtual Storage is still mounted at %s" % \
-                    self.mount)
+        #if self.mount != "":
+        #    if status != "loggedout":
+        #        print ("The user's Virtual Storage is mounted at %s" % self.mount)
+        #    else:
+        #        print ("The last user's Virtual Storage is still mounted at %s" % \
+        #            self.mount)
             
+
+    def whoami(self):
+        '''
+        Print the current active user.
+        '''
+        print self.user
+
+################################################
+#  Storage Manager Tasks
+################################################
         
-    def ls(self, file='vos://', format='ascii'):
+        
+    def ls(self, name='vos://', format='csv'):
         '''
         The list command method
         '''
@@ -106,10 +127,32 @@ class Dldo:
         if not authClient.isValidToken(self.token):
             raise Exception, "Invalid user name and/or password provided. Please try again."
         # Run the LS command
-        return storeClient.ls (self.token, name=file, format=format)
+        return storeClient.ls (self.token, name=name, format=format)
 
-    
-    def mv(self, token, source, destination, verbose=True):
+
+    def get(self, source, destination, verbose=True):
+        '''
+        Get one or more files from Data Lab.
+        '''
+        # Check that we have a good token
+        if not authClient.isValidToken(self.token):
+            raise Exception, "Invalid user name and/or password provided. Please try again."
+        # Run the GET command
+        storeClient.get (self.token, fr=source, destination,
+                            verbose=verbose)
+
+    def put(self, source, destination, verbose=True):
+        '''
+        Put files into Data Lab.
+        '''
+        # Check that we have a good token
+        if not authClient.isValidToken(self.token):
+            raise Exception, "Invalid user name and/or password provided. Please try again."
+        # Run the PUT command
+        storeClient.put (self.token, source, to=destination,
+                            verbose=verbose)
+        
+    def mv(self, source, destination, verbose=True):
         '''
         The move command method
         '''
@@ -120,4 +163,77 @@ class Dldo:
         storeClient.mv (self.token, fr=source, to=destination,
                         verbose=verbose)
 
+    def cp(self, source, destination, verbose=True):
+        '''
+        Copy a file in Data Lab
+        '''
+        # Check that we have a good token
+        if not authClient.isValidToken(self.token):
+            raise Exception, "Invalid user name and/or password provided. Please try again."
+        # Run the CP command
+        storeClient.cp (self.token, fr=source, to=destination,
+                        verbose=verbose)
+
+    def rm(self, name, verbose=True):
+        '''
+        Delete files in Data Lab
+        '''
+        # Check that we have a good token
+        if not authClient.isValidToken(self.token):
+            raise Exception, "Invalid user name and/or password provided. Please try again."
+        # Run the RM command
+        storeClient.rm (self.token, name=name, verbose=verbose)
+
         
+    def ln(self, source, target):
+        '''
+        Link a file in Data Lab
+        '''
+        # Check that we have a good token
+        if not authClient.isValidToken(self.token):
+            raise Exception, "Invalid user name and/or password provided. Please try again."
+        # Run the LN command
+        storeClient.ln (self.token, fr=source, target=target)
+
+        
+    def tag(self, name, tag):
+        '''
+        Tag a file in Data Lab
+        '''
+        # Check that we have a good token
+        if not authClient.isValidToken(self.token):
+            raise Exception, "Invalid user name and/or password provided. Please try again."
+        # Run the TAG command
+        storeClient.tag (self.token, name=name, tag=tag)
+
+    def mkdir(self, name):
+        ''' 
+        Create a directory in Data Lab
+        '''
+        # Check that we have a good token
+        if not authClient.isValidToken(self.token):
+            raise Exception, "Invalid user name and/or password provided. Please try again."
+        # Run the MKDIR command
+        storeClient.mkdir (self.token, name=name)
+
+
+    def rmdir(self, name):
+        ''' 
+        Delete a directory in Data Lab
+        '''
+        # Check that we have a good token
+        if not authClient.isValidToken(self.token):
+            raise Exception, "Invalid user name and/or password provided. Please try again."
+        # Run the RMDIR command
+        storeClient.rmdir (self.token, name=name)
+
+    def resolve(self, name):
+        ''' 
+        Resolve a vos short form identifier     -- FIXME
+        '''
+        # Check that we have a good token
+        if not authClient.isValidToken(self.token):
+            raise Exception, "Invalid user name and/or password provided. Please try again."
+        # Run the command
+        r = requests.get(SM_URL + "resolve?name=%s" %
+                         name, headers={'X-DL-AuthToken': self.token})

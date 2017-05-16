d3f4b7b86d8d8659976ed2525f654060f0e0bd54
David Nidever <dnidever@lsst.org>
2017-05-16 13:35:35 -0700

    updates

--- dl/dlinterface.py
+++ dl/dlinterface.py
@@ -850,10 +850,10 @@ class Dlinterface:
             print "No queries made so far"
         else:
             keys = sorted(self.qhistory.keys())
-            asyncv = ()
+            asyncv = []
             for k in keys:
                 v = self.qhistory[k]
-                asyncv.append(v[2])
+                asyncv.append(v[2])      # add async/sync values
                 # qid, type, async, query, time, jobid, username, format, status/nrows
                 # get the query status for ASYNC queries
                 if (async is True and v[2] == 'async') or (async is False):
@@ -861,6 +861,8 @@ class Dlinterface:
                            (v[0], strftime('%Y-%m-%d %H:%M:%S', localtime(v[4])), v[1], 'ASYNC' if v[2] else 'SYNC', v[7], 
                             str(v[8]), v[5] if v[2] else "--", v[3]))
                 # Maybe leave off the jobid if we are using QID instead??!!
+            # No async queries?
+            #if async is True and 
             print asyncv
                 
                 

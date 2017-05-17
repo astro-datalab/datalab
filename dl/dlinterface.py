d4081a22c4413a6b7731ea132517679d36749dff
David Nidever <dnidever@lsst.org>
2017-05-16 23:20:43 -0700

    updates

--- dl/dlinterface.py
+++ dl/dlinterface.py
@@ -1584,8 +1584,6 @@ class Dlinterface:
         # They require vos:// prefix to work
         lnk = (link if link.startswith("vos://") else ("vos://" + link))
         trg = (target if target.startswith("vos://") else ("vos://" + target))
-        print ("lnk = %s" % lnk)
-        print ("trg = %s" % trg)
         # Run the LN command
         storeClient.ln (token, fr=lnk, target=trg)
 

258fc6d0d6aaa775679b40e60bd51b68773f8748
Mike Fitzpatrick <fitz@noao.edu>
2017-05-22 16:24:19 -0700

    fixed service url

--- dl/queryClient.py
+++ dl/queryClient.py
@@ -33,8 +33,7 @@ import json
 #####################################
 
 
-DEF_SERVICE_URL = "http://dldev.datalab.noao.edu/query"
-#DEF_SERVICE_URL = "http://dlsvcs.datalab.noao.edu/query"
+DEF_SERVICE_URL = "http://dlsvcs.datalab.noao.edu/query"
 SM_SERVICE_URL = "http://dlsvcs.datalab.noao.edu/storage"
 PROFILE = "default"
 DEBUG = False

f4a39eb5b19a14ed17b477db1ea59a4ba4de68d0
David Nidever <dnidever@lsst.org>
2017-06-07 17:24:24 -0500

    Updated test_storeClient.py copy and move unit tests to use more basic level HTTP service calls.

--- tests/test_storeClient.py
+++ tests/test_storeClient.py
@@ -1,115 +1,207 @@
 #!/usr/bin/env python
 # D. Nidever
 #
-# testdlinterface.py
-# Python code to (unit) test the DL Client tools
+# test_storeClient.py
+# Python code to (unit) test the DL storage client code
 
-from dl import authClient, storeClient, queryClient
+from dl import storeClient
 import os
 import unittest
+import requests
 
+# Service URLs
+AM_URL = "http://dlsvcs.datalab.noao.edu/auth"      # Auth Manager
+SM_URL = "http://dlsvcs.datalab.noao.edu/storage"   # Storage Manager
+QM_URL = "http://dlsvcs.datalab.noao.edu/query"     # Query Manager
+# Test token
+TEST_TOKEN = "dltest.99998.99998.test_access"
+
+# Create test data sample
+testdata = 'id,ra,dec\n'\
+           '77.1096574,150.552192729936,-32.7846851370221\n'\
+           '77.572838,150.55443538686,-32.7850014657006\n'
+
+def fileExists(name):
+  url = SM_URL + "/ls?name=vos://%s&format=%s" % (name, 'raw')
+  r = requests.get(url, headers={'X-DL-AuthToken': TEST_TOKEN})
+  res = r.content.decode('utf-8')
+  return (True if (name in res) else False)
+  
+def get(fr,to=None):
+  url = requests.get(SM_URL + "/get?name=%s" % ('vos://'+fr),
+                     headers={"X-DL-AuthToken": TEST_TOKEN})
+  r = requests.get(url.text, stream=True)
+  if to is not None:
+    with open(to, 'wb', 0) as fd:
+      fd.write(r.raw.data)
+    fd.close()
+  else:
+    return r.raw.data
+    
+def put(fr,to):
+  r = requests.get(SM_URL + "/put?name=%s" % 'vos://'+to, headers = {"X-DL-AuthToken": TEST_TOKEN})
+  with open(fr, 'rb') as file:
+    requests.put(r.content, data=file,
+                 headers={'Content-type': 'application/octet-stream',
+                          'X-DL-AuthToken': TEST_TOKEN})
+
+def rm(name):
+  name = (name if name.startswith('vos://') else ('vos://'+name))
+  path = "/rm?file=%s" % name
+  resp = requests.get("%s%s" % (SM_URL, path), headers = {"X-DL-AuthToken": TEST_TOKEN})
+  
 def suite():
   suite = unittest.TestSuite()
-  # authClient
-  #  login, logout, isValidUser, isValidToken
   # storeClient
   #  get, put, load, cp, ln, ls, mkdir, mv, rm, rmdir, saveAs, 
-  # queryClient
-  #  query, status, results, list_profiles, list, schema, drop
   suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestCopy))
   suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestMove))
   return suite
 
+class TestGet(unittest.TestCase):
+
+    def setUp(self):
+
+    def tearDown(self):
+      
+    def test_copy(self):
+
+class TestPut(unittest.TestCase):
+
+    def setUp(self):
+
+    def tearDown(self):
+      
+    def test_copy(self):
+
+class TestLoad(unittest.TestCase):
+
+    def setUp(self):
+
+    def tearDown(self):
+      
+    def test_copy(self):
+      
 class TestCopy(unittest.TestCase):
 
     def setUp(self):
         self.file = 'test.csv'
         self.outfile = 'test2.csv'
-        # Create sample test file locally
-        data = 'id,ra,dec\n'\
-                '77.1096574,150.552192729936,-32.7846851370221\n'\
-                '77.572838,150.55443538686,-32.7850014657006\n'
+        self.testdata = testdata        
         fh = open(self.file,'wb')
-        fh.write(data)
+        fh.write(testdata)
         fh.close()
-        self.data = data
-        # Login to the dltest account
-        token = authClient.login('dltest','datalab')        
-        self.token = token
         # Delete input file if it exists already
-        if storeClient.ls(self.token,self.file) != '':
-            storeClient.rm(self.token,self.file)
+        if fileExists(self.file):
+          rm(self.file)
         # Delete output file if it exists already
-        if storeClient.ls(self.token,self.outfile) != '':
-            storeClient.rm(self.token,self.outfile)
+        if fileExists(self.outfile):
+          rm(self.outfile)
         # Put local file to VOSpace
-        storeClient.put(self.token,self.file,self.file)
+        put(self.file,self.file)
         # Delete temporary local test file
         os.remove(self.file)
-        
+            
     def tearDown(self):
-        # Delete temporary files in VOSpace
-        storeClient.rm(self.token,self.file)
-        storeClient.rm(self.token,self.outfile)
+        # Delete input file in VOSpace
+        rm(self.file)
+        # Delete output file in VOSpace
+        rm(self.outfile)
       
     def test_copy(self):
         # Try copying the file
         storeClient.cp(self.token,self.file,self.outfile)
-        res = storeClient.ls(self.token,self.outfile)
-        self.assertEqual(res,self.outfile)
-        # Copy back to local and read in
-        storeClient.get(self.token,self.file,self.file)
-        f = open(self.file,'r')
-        fdata = f.read()
-        f.close()
-        os.remove(self.file)
-        self.assertEqual(self.data,fdata)
+        # Check that the file is there
+        self.assertEqual(True,fileExists(self.outfile))
+        # Copy back to local with get
+        outdata = get(self.outfile)
+        # Make sure they are equal
+        self.assertEqual(self.testdata,outdata)
 
 class TestMove(unittest.TestCase):
 
     def setUp(self):
         self.file = 'test.csv'
         self.outfile = 'test2.csv'
-        # Create sample test file locally
-        data = 'id,ra,dec\n'\
-                '77.1096574,150.552192729936,-32.7846851370221\n'\
-                '77.572838,150.55443538686,-32.7850014657006\n'
+        self.testdata = testdata        
         fh = open(self.file,'wb')
-        fh.write(data)
+        fh.write(testdata)
         fh.close()
-        self.data = data
-        # Login to the dltest account
-        token = authClient.login('dltest','datalab')        
-        self.token = token
         # Delete input file if it exists already
-        if storeClient.ls(self.token,self.file) != '':
-            storeClient.rm(self.token,self.file)
+        if fileExists(self.file):
+          rm(self.file)
         # Delete output file if it exists already
-        if storeClient.ls(self.token,self.outfile) != '':
-            storeClient.rm(self.token,self.outfile)
+        if fileExists(self.outfile):
+          rm(self.outfile)
         # Put local file to VOSpace
-        storeClient.put(self.token,self.file,self.file)
+        put(self.file,self.file)
         # Delete temporary local test file
         os.remove(self.file)
-        
+            
     def tearDown(self):
-        # Delete temporary files in VOSpace
-        storeClient.rm(self.token,self.file)
-        storeClient.rm(self.token,self.outfile)
+        # Delete input file in VOSpace
+        rm(self.file)
+        # Delete output file in VOSpace
+        rm(self.outfile)
       
     def test_move(self):
-        # Try moving the file
+        # Try copying the file
         storeClient.mv(self.token,self.file,self.outfile)
-        res = storeClient.ls(self.token,self.outfile)
-        self.assertEqual(res,self.outfile)
-        # Copy back to local and read in
-        storeClient.get(self.token,self.outfile,self.outfile)
-        f = open(self.outfile,'r')
-        fdata = f.read()
-        f.close()
-        os.remove(self.outfile)
-        self.assertEqual(self.data,fdata)
+        # Check that the file is there
+        self.assertEqual(True,fileExists(self.outfile))
+        # Get the file contents
+        outdata = get(self.outfile)
+        # Make sure they are equal
+        self.assertEqual(self.testdata,outdata)
 
+class TestRemove(unittest.TestCase):
+
+    def setUp(self):
+
+    def tearDown(self):
+      
+    def test_copy(self):
+        
+class TestLink(unittest.TestCase):
+
+    def setUp(self):
+
+    def tearDown(self):
+      
+    def test_copy(self):
+
+class TestList(unittest.TestCase):
+
+    def setUp(self):
+
+    def tearDown(self):
+      
+    def test_copy(self):
+
+class TestMkdir(unittest.TestCase):
+
+    def setUp(self):
+
+    def tearDown(self):
+      
+    def test_copy(self):
+
+class TestRmdir(unittest.TestCase):
+
+    def setUp(self):
+
+    def tearDown(self):
+      
+    def test_copy(self):
+
+class TestSaveas(unittest.TestCase):
+
+    def setUp(self):
+
+    def tearDown(self):
+      
+    def test_copy(self):
+      
 
 # query with WHERE clause with SORT BY and LIMIT
         

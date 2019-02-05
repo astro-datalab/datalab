b79eac9f463d7ddc3348b6b9d635d1386c344972
Mike Fitzpatrick <fitz@noao.edu>
2019-02-05 17:36:48 +0000

    added pandas requirement

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(name="noaodatalab",
           'Programming Language :: Python',
           'Topic :: Scientific/Engineering :: Astronomy',
         ], 
-      install_requires=['requests>=2.7', 'httplib2', 'numpy', 'astropy', 'pyvo', 'matplotlib'],
-      requires=['requests (>=2.7)', 'httplib2', 'numpy', 'astropy', 'pyvo', 'matplotlib']
+      install_requires=['requests>=2.7', 'httplib2', 'numpy', 'astropy', 'pyvo', 'matplotlib','pandas'],
+      requires=['requests (>=2.7)', 'httplib2', 'numpy', 'astropy', 'pyvo', 'matplotlib','pandas']
       )

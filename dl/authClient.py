45a9156cbafc92a4e7dade1d0f80c59f49d5c45d
Igor Suarez-Sola <igor.suarez-sola@noirlab.edu>
2022-10-13 12:58:18 -0700

    Adding utility token functions #54
    This commit is linked to issue #54
    
    Three token manipulation functions have been added to the Util.py
    package namely:
    
    .- "parse_auth_token: runs a regular expression on a string and returns a
                          regex Match object or nothing
    .- split_auth_token: takes the Matched object and converts it into an
                         array
    .- is_auth_token: returns True if parse_auth_token returns a match or
                      False otherwise
    
    Added unit test tests/test_util.py to test split_auth_token and
    is_auth_token

--- dl/authClient.py
+++ dl/authClient.py
@@ -59,7 +59,7 @@ except ImportError:
 #    from Util import def_token
 #else:                                           # use distribution copy
 #    from dl.Util import def_token
-from dl.Util import def_token
+from dl.Util import def_token, split_auth_token
 
 is_py3 = sys.version_info.major == 3
 
@@ -156,7 +156,7 @@ def whoAmI():
     user = 'anonymous'
     try:
         token = def_token(None)
-        user, uid, gid, hash = token.strip().split('.', 3)
+        user, uid, gid, hash = split_auth_token(token.strip())
     except:
         return 'anonymous'
     else:
@@ -173,7 +173,7 @@ def isAlive(svc_url=DEF_SERVICE_URL):
 
 def isValidToken(token):
     try:
-        user, uid, gid, hash = token.strip().split('.', 3)
+        user, uid, gid, hash = split_auth_token(token.strip())
     except Exception:
         return False
 
@@ -370,7 +370,7 @@ class authClient(object):
         user = 'anonymous'
         try:
             token = def_token(None)
-            user, uid, gid, hash = token.strip().split('.', 3)
+            user, uid, gid, hash = split_auth_token(token.strip())
         except:
             return 'anonymous'
         else:
@@ -687,7 +687,7 @@ class authClient(object):
             print("logout: logged in = " + self.isTokenLoggedIn(token))
 
         try:
-            user, uid, gid, hash = token.strip().split('.', 3)
+            user, uid, gid, hash = split_auth_token(token.strip())
         except Exception:
             raise dlAuthError('Error: Invalid user token')
 
@@ -764,7 +764,7 @@ class authClient(object):
         # Reset the auth_token to the one passed in by the service call.
         self.auth_token = token
 
-        user, uid, gid, hash = self.auth_token.strip().split('.', 3)
+        user, uid, gid, hash = split_auth_token(self.auth_token.strip())
         if user != 'root' and user != username:
             raise Exception("Error: Invalid user or non-root token")
 
@@ -1044,7 +1044,7 @@ class authClient(object):
         '''Get default tracking headers.
         '''
         tok = def_token(token)
-        user, uid, gid, hash = tok.strip().split('.', 3)
+        user, uid, gid, hash = split_auth_token(tok.strip())
         hdrs = {'Content-Type': 'text/ascii',
                 'X-DL-ClientVersion': __version__,
                 'X-DL-OriginIP': self.hostip,

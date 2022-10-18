71b60d1ad29db1c7d924b5a03ec2e6e3272187de
Igor Suarez-Sola <igor.suarez-sola@noirlab.edu>
2022-10-18 16:02:13 -0700

    obscuring tokens

--- tests/test_util.py
+++ tests/test_util.py
@@ -22,10 +22,10 @@ import dl.Util as util
 ANON_TOKEN = "anonymous.0.0.anon_access"
 DEMO_TOKEN = "dldemo.99999.99999.demo_access"
 TEST_TOKEN = "dltest.99998.99998.test_access"
-DEMO00_OK_TOKEN = "demo00.1018.1018.$1$fdqe8g1I$jN1F2CZ8MhdbZNnuG9jAB/"
-DEMO00_KO_TOKEN = "demo00.1018.1018.$2$fdqe8g1I$jN1F2CZ8MhdbZNnuG9jAB/"
-DEMO00_TOO_SHORT = "demo00.1018.1018.$1$fdqe8g1I$jN1F2CZ8Mhdb"
-SOME_USER = "some_user.3195.159.$1$3xf8E6xf$RL9XLqIyFCRsTOISOBTuL."
+UNITTEST_OK_TOKEN = "unittest.666.666.$1$fdqasldur927JL97asldfj9279172B/"
+UNITTEST_KO_TOKEN = "unittest.666.666.$2$fdqasldur927JL97asldfj9279172B/"
+UNITTEST_TOO_SHORT = "unittest.666.666.$1$fdqasldur927JL97asldf"
+SOME_USER = "some_user.3199.159.$1$3xf8E6xf$RL9XLqIyFCRsTOISOBTuL."
 
 
 @pytest.mark.parametrize(
@@ -35,9 +35,9 @@ SOME_USER = "some_user.3195.159.$1$3xf8E6xf$RL9XLqIyFCRsTOISOBTuL."
        (ANON_TOKEN, True),
        (DEMO_TOKEN, True),
        (TEST_TOKEN, True),
-       (DEMO00_OK_TOKEN, True),
-       (DEMO00_KO_TOKEN, False),
-       (DEMO00_TOO_SHORT, False),
+       (UNITTEST_OK_TOKEN, True),
+       (UNITTEST_KO_TOKEN, False),
+       (UNITTEST_TOO_SHORT, False),
 
    ]
 )
@@ -52,8 +52,8 @@ def test_is_auth_token(token, expected):
         (ANON_TOKEN, ["anonymous", "0", "0", "anon_access"]),
         (DEMO_TOKEN, ["dldemo", "99999", "99999", "demo_access"]),
         (TEST_TOKEN, ["dltest", "99998", "99998", "test_access"]),
-        (DEMO00_OK_TOKEN, ["demo00", "1018", "1018", "$1$fdqe8g1I$jN1F2CZ8MhdbZNnuG9jAB/"]),
-        (SOME_USER, ["some_user", "3195", "159", "$1$3xf8E6xf$RL9XLqIyFCRsTOISOBTuL."])
+        (UNITTEST_OK_TOKEN, ["unittest", "666", "666", "$1$fdqasldur927JL97asldfj9279172B/"]),
+        (SOME_USER, ["some_user", "3199", "159", "$1$3xf8E6xf$RL9XLqIyFCRsTOISOBTuL."])
     ]
 )
 def test_split_auth_token_parts(token, expected):
